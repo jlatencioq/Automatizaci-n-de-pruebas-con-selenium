8f8c8db9f0c806c43a103285d72c55e677b50137
JAQGO <106003002+JAQGO@users.noreply.github.com>
2022-08-07 13:33:05 -0500

    usando  unittest
    pyton

--- miSelenium.py
+++ miSelenium.py
@@ -1,38 +1,46 @@
-
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome("C:\\Users\\JAQ\Documents\\PROYECTO\\chromedriver_win32\\chromedriver")
+class TestResultPercentageCalculator(unittest.TestCase):
+    def setUp(self):
+        pass
+      
+    def test_strings_a(self):
+        driver = webdriver.Chrome("C:\\Users\\JAQ\Documents\\PROYECTO\\chromedriver_win32\\chromedriver")
 
-# Puts an Implicit wait, Will wait for 10 seconds before throwing exception
-driver.implicitly_wait(0.5)
+        # Puts an Implicit wait, Will wait for 10 seconds before throwing exception
+        driver.implicitly_wait(0.5)
 
-# Launch website
-driver.get("https://www.calculator.net")
+        # Launch website
+        driver.get("https://www.calculator.net")
 
+        # Click on Math Calculators
+        driver.find_element(By.XPATH, "//*[@id=\"contentout\"]/table/tbody/tr/td[3]/div[2]/a").click()
 
+        # Click on Percent Calculators
+        driver.find_element(By.XPATH, "//*[@id=\"content\"]/table[2]/tbody/tr/td/div[3]/a").click()
 
-# Click on Math Calculators
-driver.find_element(By.XPATH, "//*[@id=\"contentout\"]/table/tbody/tr/td[3]/div[2]/a").click()
+        # Enter value 10 in the first number of the percent Calculator
+        driver.find_element(By.ID, "cpar1").send_keys("10")
 
-# Click on Percent Calculators
-driver.find_element(By.XPATH, "//*[@id=\"content\"]/table[2]/tbody/tr/td/div[3]/a").click()
+        # Enter value 50 in the second number of the percent Calculator
+        driver.find_element(By.ID, "cpar2").send_keys("50")
 
-# Enter value 10 in the first number of the percent Calculator
-driver.find_element(By.ID, "cpar1").send_keys("10")
+        # Click Calculate Button
+        driver.find_element(By.XPATH, "//*[@id = 'content']/table/tbody/tr[2]/td/input[2]").click()
 
-# Enter value 50 in the second number of the percent Calculator
-driver.find_element(By.ID, "cpar2").send_keys("50")
+        # Get the Result Text based on its xpath
+        result = driver.find_element(By.XPATH, "//*[@id = 'content']/p[2]/font/b").text
 
-# Click Calculate Button
-driver.find_element(By.XPATH, "//*[@id = 'content']/table/tbody/tr[2]/td/input[2]").click()
+        # Print a Log In message to the screen
+        # print(" The Result is " + result) 
 
+        self.assertEqual( '5', result)
 
+        driver.quit() 
+    
 
-# Get the Result Text based on its xpath
-result = driver.find_element(By.XPATH, "//*[@id = 'content']/p[2]/font/b").text
+if __name__ == '__main__':
 
-# Print a Log In message to the screen
-print(" The Result is " + result)
-
-driver.quit()
+    unittest.main()
